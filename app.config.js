1dee436fa3e04f9fa84ef22802647e2130b9ac40
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-10-25 11:44:22 +0200

    :rocket: [Bump] Version to 207

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ dotenv.config();
 export default ({ config }) => ({
   ...config,
   ios: {
-    buildNumber: '100000206',
+    buildNumber: '100000207',
     bundleIdentifier: 'com.reconnect.CloudSolidaire' + process.env.EXPO_PUBLIC_ENV,
     infoPlist: { 
       UIBackgroundModes: ['audio']
@@ -17,7 +17,7 @@ export default ({ config }) => ({
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff'
       },
-      versionCode: '100000206',
+      versionCode: '100000207',
       package: 'com.reconnect.CloudSolidaire' + process.env.EXPO_PUBLIC_ENV,
       googleServicesFile: './android/google-services.json'
     },
