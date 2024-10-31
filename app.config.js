59156bf6c562d04f948ad289ed901cdb781e2e8d
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-10-31 12:16:46 +0100

    :wrench: [Android Config] Remove a few duplicated data

--- app.config.js
+++ app.config.js
@@ -6,7 +6,6 @@ dotenv.config();
 export default ({ config }) => ({
   ...config,
   ios: {
-    bundleIdentifier: 'com.reconnect.CloudSolidaire' + process.env.EXPO_PUBLIC_ENV,
     infoPlist: { 
       UIBackgroundModes: ['audio']
     }
@@ -16,8 +15,6 @@ export default ({ config }) => ({
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#ffffff'
     },
-    versionCode: 100000207,
-    package: 'com.reconnect.CloudSolidaire' + process.env.EXPO_PUBLIC_ENV,
     googleServicesFile: './android/google-services.json',
   },
   updates: {
