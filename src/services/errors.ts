b8ea428dec3afba52a2b4315b1dab655a7d16ea3
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2025-09-09 15:43:49 +0200

    fix: fallback to the old handle error management

--- src/services/errors.ts
+++ src/services/errors.ts
@@ -1,12 +1,10 @@
 import { AxiosError } from 'axios';
 import { Alert } from 'react-native';
-import t from './translation';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as RootNavigation from '../RootNavigation';
+import t from './translation';
 
 export const handle401 = (): void => {
   RootNavigation.navigate('Auth');
-  AsyncStorage.removeItem('accessToken');
 };
 
 export const handleError = (error: AxiosError): void => {
